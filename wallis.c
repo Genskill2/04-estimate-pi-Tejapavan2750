88634982b82f406e0476c67fc50edf6d7c7f9cac
Tejapavan2750 <84578336+Tejapavan2750@users.noreply.github.com>
2021-06-03 20:39:37 +0530

    It is better to fail in originality than to succeed in imitation.

--- wallis.c
+++ wallis.c
@@ -3,11 +3,21 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
-
-int main(void) {
+ float wallis_pi(int x) 
+ // x is taken in referance of count
+ {
+    float pi = 1;
+    for (int a = 1; a < x; a++)
+    {
+        float num = 4.0 * a * a;
+        pi *= (num / (num - 1));
+    }
+    return pi * 2 ;
+    }
+    
+int main() {
   float pi;
-  for (int i=0; i<5; i++) {
+  for (int i= 1; i<5; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
@@ -23,4 +33,3 @@ int main(void) {
     }
   }
 }
-
